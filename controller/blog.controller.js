9e4337f1de1f4391cc355f26ce64404b9b71325f
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-08-28 21:23:00 +0530

    socket add

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -1,22 +1,30 @@
 const asyncHandler = require("express-async-handler")
 const Blog = require("../models/Blog")
+const { io } = require("../socket/Socket")
 
 exports.fetchBlog= asyncHandler(async(req,res)=>{
- const result = await Blog.find()
+    const result = await Blog.find()
+    io.emit("blog-create", result) 
     res.json({message:"Blog Fetch Success", result})
 })
 
 exports.createBlog = asyncHandler(async(req,res)=>{
     await Blog.create(req.body)
+    const result = await Todo.find()
+    io.emit("blog-create", result) 
     res.json({message:"Blog Create Success"})
 })
 
 
 exports.updateBlog= asyncHandler(async(req,res)=>{
     await Blog.findByIdAndUpdate(req.params.id, req.body)
+    const result = await Blog.find()
+    io.emit("blog-create", result) 
     res.json({message:"Blog Update Success"})
 })
 exports.deleteBlog= asyncHandler(async(req,res)=>{
     await Blog.findByIdAndDelete(req.params.id)
+    const result = await Blog.find()
+    io.emit("blog-create", result) 
     res.json({message:"Blog Delete Success"})
 })
